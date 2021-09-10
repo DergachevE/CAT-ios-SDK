9f32d153101e8f2a0dea1a7ad3e0cb69e57dc7f2
Evgenii Dergachev <edergachev@microsoft.com>
2021-09-10 22:41:16 +0200

    Initial commit

---
+++ Meow/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Meow
+//
+//  Created by Jessica Yeh on 5/31/17.
+//  Copyright © 2017 Jessica Yeh. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    static let intInstallUrl = "https://install.appcenter-int.trafficmanager.net"
+    static let intApiUrl = "https://appcenter-int.trafficmanager.net/api/v0.1"
+    static let intAppSecret = "b22f463d-a561-405e-8579-161977402dd0"
+    static let accessibilityIntAppSecret = "5f46a336-bd46-4a2d-b14c-6a82b35dba16"
+    
+    static let stagingInstallUrl = "https://install.appcenter-staging.trafficmanager.net"
+    static let stagingApiUrl = "https://appcenter-staging.trafficmanager.net/api/v0.1"
+    static let stagingAppSecret = "b11966f5-0aa6-4d12-8b29-6da1c6cecf44"
+    
+    static let prodAppSecret = "45842f5c-b706-4ba8-8229-009ca0730b97"
+}
---
+++ Pods/Target Support Files/Pods-Meow-INT/Pods-Meow-INT-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Meow_INTVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Meow_INTVersionString[];
+
